2d67d12a7cfa67a25a778b26fc4606f1ad8a0640
amirkle27 <amirkle@gmail.com>
2024-08-06 19:41:59 +0300

    Add files via upload

---
+++ Salary.py
@@ -0,0 +1,5 @@
+per_hour = float (input("Hello Honey, Can I please ask how much you make an hour?"))
+working_hours_normal = float (input("And how many working hours have you had this month? (Shabbath NOT included)"))
+working_hours_Shabbath = float (input("And lastly, how many working hours ON SHABBATH have you been had this month, dear?"))
+salary = float (working_hours_normal*per_hour)+(working_hours_Shabbath*per_hour*1.5)
+print(f"Your Salary for this month should be: {salary} ")
